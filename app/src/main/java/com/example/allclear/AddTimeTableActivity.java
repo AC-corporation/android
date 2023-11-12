b5b15e2ade2a7a079a8c69cf7256d09b196d3116
fprtmjinho <fjrtmjinho@gmail.com>
2023-11-12 18:12:13 +0900

    feat: 피그마 좌측 상단부터 피그마 좌측 하단까지 액티비티, 프래그먼트 생성

---
+++ app/src/main/java/com/example/allclear/AddTimeTableActivity.java
@@ -0,0 +1,14 @@
+package com.example.allclear;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddTimeTableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_time_table);
+    }
+}
---
+++ app/src/main/java/com/example/allclear/MakeTimeTableActivity.java
@@ -0,0 +1,14 @@
+package com.example.allclear;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MakeTimeTableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_time_table);
+    }
+}
