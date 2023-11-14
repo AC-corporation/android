b180549079e0773fc4226ac5f58dda0a07e3b87b
fprtmjinho <fjrtmjinho@gmail.com>
2023-11-14 20:43:36 +0900

    feat: 스플레쉬 화면 제작

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.allclear"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.allclear"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/example/allclear/LoginActivity.java
+++ app/src/main/java/com/example/allclear/LoginActivity.java
@@ -9,6 +9,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.Theme_AllClear);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
