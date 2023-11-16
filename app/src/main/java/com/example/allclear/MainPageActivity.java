69012835477fec6b5f9c8bf99ffcb8edaaa8041a
fprtmjinho <fjrtmjinho@gmail.com>
2023-11-17 00:05:25 +0900

    feat: MainPageActivity.java - 바텀 네비게이션바 연결

--- app/src/main/java/com/example/allclear/MainPageActivity.java
+++ app/src/main/java/com/example/allclear/MainPageActivity.java
@@ -1,10 +1,13 @@
 package com.example.allclear;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.allclear.databinding.ActivityMainPageBinding;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainPageActivity extends AppCompatActivity {
     private ActivityMainPageBinding binding;
@@ -13,5 +16,28 @@ public class MainPageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainPageBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        initBottomNavigation();
+    }
+    private void initBottomNavigation(){
+        NavigationBarView navigationBarView = binding.bottomNavigationview;
+        navigationBarView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.timeTable) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.containers, new TimeTableFragment()).commit();
+                    return true;
+                } else if (item.getItemId() == R.id.gr) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.containers, new GradeFragment()).commit();
+                    return true;
+                } else if (item.getItemId() == R.id.credit) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.containers, new GraduationFragment()).commit();
+                    return true;
+                } else if (item.getItemId() == R.id.myPage) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.containers, new MyPageFragment()).commit();
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }
