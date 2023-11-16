708b29bf68b6cbd9177b1b8e4d2b8c25f4552953
fprtmjinho <fjrtmjinho@gmail.com>
2023-11-17 00:21:02 +0900

    fix: LoginActivity.java - 학번, 비밀번호 비어있을때 버튼 비활성화 수정

--- app/src/main/java/com/example/allclear/LoginActivity.java
+++ app/src/main/java/com/example/allclear/LoginActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -19,7 +22,8 @@ public class LoginActivity extends AppCompatActivity {
         setTheme(R.style.Theme_AllClear);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        btnEnabled();
+        binding.loginBtn.setEnabled(false);
+        editChanged();
         binding.loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -29,9 +33,44 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
     //학번, 비밀번호 비어있을때 버튼 비활성화
-    private void btnEnabled(){
-        if(binding.loginStuedntidEt.toString().equals("") || binding.loginPasswordEt.toString().equals(""))binding.loginBtn.setEnabled(false);
-        else binding.loginBtn.setEnabled(true);
+    private void editChanged(){
+        binding.loginStuedntidEt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(binding.loginStuedntidEt.getText().toString().equals("")||binding.loginPasswordEt.getText().toString().equals("")) binding.loginBtn.setEnabled(false);
+                else binding.loginBtn.setEnabled(true);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(binding.loginStuedntidEt.getText().toString().equals("")||binding.loginPasswordEt.getText().toString().equals("")) binding.loginBtn.setEnabled(false);
+                else binding.loginBtn.setEnabled(true);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(binding.loginStuedntidEt.getText().toString().equals("")||binding.loginPasswordEt.getText().toString().equals("")) binding.loginBtn.setEnabled(false);
+                else binding.loginBtn.setEnabled(true);
+            }
+        });
+        binding.loginPasswordEt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(binding.loginPasswordEt.getText().toString().equals("")||binding.loginStuedntidEt.getText().toString().equals("")) binding.loginBtn.setEnabled(false);
+                else binding.loginBtn.setEnabled(true);
+            }
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(binding.loginPasswordEt.getText().toString().equals("")||binding.loginStuedntidEt.getText().toString().equals("")) binding.loginBtn.setEnabled(false);
+                else binding.loginBtn.setEnabled(true);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(binding.loginPasswordEt.getText().toString().equals("")||binding.loginStuedntidEt.getText().toString().equals("")) binding.loginBtn.setEnabled(false);
+                else binding.loginBtn.setEnabled(true);
+            }
+        });
     }
     //학번, 비밀번호를 백엔드에 전송후 토큰 받아오기
     private void loginRequest(){
@@ -41,6 +80,8 @@ public class LoginActivity extends AppCompatActivity {
         Intent intent = new Intent(getApplicationContext(), MainPageActivity.class);
         //토큰을 받아올경우 넣어주기
         //intent.putExtra("token",);
+        Log.i("id",binding.loginStuedntidEt.getText().toString());
+        Log.i("id",binding.loginPasswordEt.getText().toString());
         finish();
         startActivity(intent);
     }
