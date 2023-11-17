0f33daa939dc7a946153febb8ede950a3a8653c0
fprtmjinho <fjrtmjinho@gmail.com>
2023-11-17 23:44:25 +0900

    feat: LoginActivity.java - 학번, 비밀번호 비어있으면 버튼 비활성화(확정) - 중복으로 사용되는 코드 함수화

--- app/src/main/java/com/example/allclear/LoginActivity.java
+++ app/src/main/java/com/example/allclear/LoginActivity.java
@@ -35,61 +35,48 @@ public class LoginActivity extends AppCompatActivity {
         binding.loginStuedntidEt.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                if(emptyCheck()) binding.loginBtn.setEnabled(false);
-                else binding.loginBtn.setEnabled(true);
+                setBtnEnabled();
             }
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                if(emptyCheck()) binding.loginBtn.setEnabled(false);
-                else binding.loginBtn.setEnabled(true);
+                setBtnEnabled();
             }
 
             @Override
             public void afterTextChanged(Editable editable) {
-                if(emptyCheck()) binding.loginBtn.setEnabled(false);
-                else binding.loginBtn.setEnabled(true);
+                setBtnEnabled();
             }
         });
         binding.loginPasswordEt.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                if(emptyCheck()) binding.loginBtn.setEnabled(false);
-                else binding.loginBtn.setEnabled(true);
+                setBtnEnabled();
             }
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                if(emptyCheck()) binding.loginBtn.setEnabled(false);
-                else binding.loginBtn.setEnabled(true);
+                setBtnEnabled();
             }
 
             @Override
             public void afterTextChanged(Editable editable) {
-                if(emptyCheck()) binding.loginBtn.setEnabled(false);
-                else binding.loginBtn.setEnabled(true);
+                setBtnEnabled();
             }
         });
+    }
+    private void setBtnEnabled(){
+        if(emptyCheck()) binding.loginBtn.setEnabled(false);
+        else binding.loginBtn.setEnabled(true);
     }
     private boolean emptyCheck(){
         return binding.loginPasswordEt.getText().toString().equals("")||binding.loginStuedntidEt.getText().toString().equals("");
     }
-    private boolean loginCheck(){
-        if (binding.loginStuedntidEt.getText().toString().length() != 8){
+    private boolean loginCheck() {
+        if (binding.loginStuedntidEt.getText().toString().length() != 8) {
             Toast.makeText(this, "학번은 8자리를 입력해야합니다.", Toast.LENGTH_SHORT).show();
             return false;
-        }
-        else return true;
-        //비활성화가 아닌 Toast 띄우기일경우
-//        if (emptyCheck()){
-//            Toast.makeText(this, "학번 또는 비밀번호가 입력되지 않았습니다.", Toast.LENGTH_SHORT).show();
-//            return false;
-//        }
-//        else if(binding.loginStuedntidEt.getText().toString().length() != 8){
-//            Toast.makeText(this, "학번은 8자리를 입력해야합니다.", Toast.LENGTH_SHORT).show();
-//            return false;
-//        } else return true;
+        } else return true;
     }
-    //학번, 비밀번호를 백엔드에 전송후 토큰 받아오기
     private void loginRequest(){
 
     }
