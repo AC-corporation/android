40473e5ccefe4dcfb7b8544e1ef9e32b7a8f0140
LEE YOU BIN <youbeene3409@naver.com>
2023-11-20 16:06:41 +0900

    [ADD] 시간표 파일 추가

---
+++ app/src/main/java/com/example/allclear/maketimetable/EssentialSubjectActivity.java
@@ -0,0 +1,16 @@
+package com.example.allclear.maketimetable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.allclear.R;
+
+public class EssentialSubjectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_essential_subject);
+    }
+}
---
+++ app/src/main/java/com/example/allclear/maketimetable/SelfAddTwoActivity.java
@@ -0,0 +1,16 @@
+package com.example.allclear.maketimetable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.allclear.R;
+
+public class SelfAddTwoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_self_add_two);
+    }
+}
