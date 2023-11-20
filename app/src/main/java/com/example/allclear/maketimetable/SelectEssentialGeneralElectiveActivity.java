ed513d2c2a81e69816ba585ebe93de25989635a4
Lee-min-gue <125437775+Lee-min-gue@users.noreply.github.com>
2023-11-21 01:52:44 +0900

    [add] 시간표 생성 xml 작업

---
+++ app/src/main/java/com/example/allclear/maketimetable/SelectEssentialGeneralElectiveActivity.java
@@ -0,0 +1,16 @@
+package com.example.allclear.maketimetable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.allclear.R;
+
+public class SelectEssentialGeneralElectiveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_essential_general_elective);
+    }
+}
