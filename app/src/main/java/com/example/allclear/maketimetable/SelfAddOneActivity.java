aec5b3298cee3f95eb789ece938f4c58710b3382
LEE YOU BIN <youbeene3409@naver.com>
2023-11-21 15:05:08 +0900

    [UI] 시간표 UI

--- app/src/main/java/com/example/allclear/maketimetable/SelfAddOneActivity.java
+++ app/src/main/java/com/example/allclear/maketimetable/SelfAddOneActivity.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.allclear.R;
+import com.example.allclear.databinding.ActivitySelfAddOneBinding;
 
 public class SelfAddOneActivity extends AppCompatActivity {
 
+    private ActivitySelfAddOneBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        binding = ActivitySelfAddOneBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_self_add);
+        setContentView(binding.getRoot());
     }
 }
