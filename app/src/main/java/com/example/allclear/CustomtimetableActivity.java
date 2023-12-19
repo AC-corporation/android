2c0579259ac4ee3513e75f00abe6c7609b557680
sarahstar <star092312@gmail.com>
2023-12-19 20:30:48 +0900

    [add]customtimetable 라이브러리 연동작업&시간표 생성 및 요소 추가

---
+++ app/src/main/java/com/example/allclear/CustomtimetableActivity.java
@@ -0,0 +1,47 @@
+package com.example.allclear;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.allclear.databinding.ActivityCustomtimetableBinding;
+import com.islandparadise14.mintable.model.ScheduleDay;
+import com.islandparadise14.mintable.model.ScheduleEntity;
+
+import java.util.ArrayList;
+
+public class CustomtimetableActivity extends AppCompatActivity {
+    private ActivityCustomtimetableBinding binding;
+    private String[] day = {"Mon", "Tue", "Wen", "Thu", "Fri"};
+    private ArrayList<ScheduleEntity> scheduleList = new ArrayList<>();
+    ScheduleEntity schedule = new ScheduleEntity(
+            32,                  // originId
+            "Database",          // scheduleName
+            "IT Building 301",    // roomInfo
+            ScheduleDay.TUESDAY,  // ScheduleDay object (MONDAY ~ SUNDAY)
+            "8:20",               // startTime format: "HH:mm"
+            "10:30",              // endTime format: "HH:mm"
+            "#73fcae68",          // backgroundColor (optional)
+            "#000000"             // textColor (optional)
+    );
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivityCustomtimetableBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        scheduleList.add(schedule);
+
+    }
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        binding.table.initTable(day);
+        binding.table.updateSchedules(scheduleList);
+    }
+
+
+}
