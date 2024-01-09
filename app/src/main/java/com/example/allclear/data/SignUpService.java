2a37aa26ae3de041241afab0b69bc29aa35d3af8
Lee-min-gue <125437775+Lee-min-gue@users.noreply.github.com>
2024-01-09 14:42:15 +0900

    [FEAT]회원가입 기능 서버통신 구현중3

--- app/src/main/java/com/example/allclear/data/SignUpService.java
+++ app/src/main/java/com/example/allclear/data/SignUpService.java
@@ -1,10 +1,12 @@
 package com.example.allclear.data;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
 
 public interface SignUpService {
     @POST("user/signup")
-    Call<Void> signUp(@Body MemberSignupRequestDto memberSignupRequestDto);
+    Call<List<TestResponseDto>> signUp(@Body MemberSignupRequestDto memberSignupRequestDto);
 }
