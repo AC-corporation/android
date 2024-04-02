ea96af8552393b7aff9d8fc6aa3017928d3573e8
fprtmjinho <fjrtmjinho@gmail.com>
2024-04-02 19:51:09 +0900

    [FEAT] EditTimeTableTwoActivity.java 필터 기능 제외

--- app/src/main/java/com/example/allclear/data/service/SubjectService.java
+++ app/src/main/java/com/example/allclear/data/service/SubjectService.java
@@ -1,10 +1,10 @@
 package com.example.allclear.data.service;
 
-import com.example.allclear.data.request.SubjectSearchsRequestDto;
-import com.example.allclear.data.response.GraduationDto;
 import com.example.allclear.data.response.SubjectAllDto;
-import com.example.allclear.data.response.SubjectDto;
-import com.example.allclear.data.response.SubjectSearchsResponseDto;
+//import com.example.allclear.data.request.SubjectSearchsRequestDto;
+//import com.example.allclear.data.response.GraduationDto;
+//import com.example.allclear.data.response.SubjectDto;
+//import com.example.allclear.data.response.SubjectSearchsResponseDto;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
@@ -15,17 +15,17 @@ import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface SubjectService {
-    @GET("subject/{subjectId}")
-    Call<SubjectDto> getSubjectServer(
-            @Header("authorization") String authHeader,
-            @Path("subjectId") Long subjectId);
+//    @GET("subject/{subjectId}")
+//    Call<SubjectDto> getSubjectServer(
+//            @Header("authorization") String authHeader,
+//            @Path("subjectId") Long subjectId);
     @GET("subject/findAll")
     Call<SubjectAllDto> getSubjectAllServer(
             @Header("authorization") String authHeader,
             @Query("page") int page);
-    @POST("subject/searchs")
-    Call<SubjectSearchsResponseDto> postSubjectSearchsServer(
-            @Header("authorization") String authHeader,
-            @Path("page") int page,
-            @Body() SubjectSearchsRequestDto subjectSearchsResponseDto);
+//    @POST("subject/searchs")
+//    Call<SubjectSearchsResponseDto> postSubjectSearchsServer(
+//            @Header("authorization") String authHeader,
+//            @Path("page") int page,
+//            @Body() SubjectSearchsRequestDto subjectSearchsResponseDto);
 }
